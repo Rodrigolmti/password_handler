3be06f8c1a0bc27e51b4d83f73161d2c8ab70bd3
Rodrigo Martins <rodrigo.martins@ifood.com.br>
2020-04-18 17:20:05 -0300

    Add ktlin and git hook

--- app/src/main/kotlin/br/com/rodrigolmti/password_keeper/ui/SplashFragment.kt
+++ app/src/main/kotlin/br/com/rodrigolmti/password_keeper/ui/SplashFragment.kt
@@ -13,7 +13,8 @@ import br.com.rodrigolmti.password_keeper.R
 class SplashFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_splash, container, false)
